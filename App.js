82dda9efa01aa8ac9e37b36e1b93a97959c3b846
edison lopez <stiiven6241@gmail.com>
2023-02-25 15:30:35 -0500

    Prject intro

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container,styles.borders,{borderColor:'blue'}]}>
+      <Text style={{color:'white'}}>Hola Mundo</Text>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  borders:{
+    borderWidth:5,
+    borderRadius:30
+
+  }
 });
